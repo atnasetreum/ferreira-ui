bf098c338bc483be11b26b98d349ab7c585cd4da
Eduardo García <atnasetreum@gmail.com>
2023-01-19 16:49:52 -0600

    feat: image preview table

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost", "transportesferreira.com"],
+    domains: ["localhost", "api.transportesferreira.com"],
   },
 };
 
