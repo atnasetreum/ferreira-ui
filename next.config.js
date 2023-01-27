b414537d0391aa3eca3bc11311aeda4ef867d747
Eduardo García <atnasetreum@gmail.com>
2023-01-26 18:14:42 -0600

    feat: create route done

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: { granularChunks: true },
   images: {
     domains: ["localhost", "api.transportesferreira.com"],
   },
