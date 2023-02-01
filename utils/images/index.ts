0793a217bd7302b14cb9f5d1ab3e9ee7a56cef5e
Eduardo García <atnasetreum@gmail.com>
2023-02-01 12:27:13 -0600

    with ref

--- utils/images/index.ts
+++ utils/images/index.ts
@@ -1,3 +1,4 @@
 export const createUrlImage = (img: string, folder: string = "sellers") => {
+  //return `https://api.transportesferreira.com/static/images/${folder}/${img}`;
   return `${process.env.NEXT_PUBLIC_API_ROUTE}/static/images/${folder}/${img}`;
 };
