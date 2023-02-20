c270d7872415f9d6a1121efb33e3d505291cc73d
Eduardo García <atnasetreum@gmail.com>
2023-02-20 15:44:29 -0600

    routes done

--- ts/interfaces/route.interfaces.ts
+++ ts/interfaces/route.interfaces.ts
@@ -1,3 +1,78 @@
+// export interface Route {
+//   id: number;
+//   date: string;
+//   notes: string;
+//   ciclo: string;
+//   pago: string;
+//   isActive: boolean;
+//   createdAt: string;
+//   updatedAt: string;
+//   user: UserRoutes;
+//   car: CarRoutes;
+//   sellers: SellerRoutes[];
+// }
+
+// interface CarRoutes {
+//   id: number;
+//   placa: string;
+//   image: string;
+//   isActive: boolean;
+//   createdAt: string;
+//   updatedAt: string;
+//   logistica: UserRoutes;
+// }
+
+// interface UserRoutes {
+//   id: number;
+//   name: string;
+//   isActive: boolean;
+//   createdAt: string;
+//   updatedAt: string;
+//   password?: string;
+// }
+
+// interface SellerRoutes {
+//   id: number;
+//   uuid: string;
+//   nombre: string;
+//   calle: string;
+//   numero: string;
+//   colonia: string;
+//   ciudad: string;
+//   municipio: string;
+//   estado: string;
+//   cp: string;
+//   linkUbicacion: string;
+//   image: string;
+//   personaQueAtiende: null;
+//   isActive: boolean;
+//   createdAt: string;
+//   updatedAt: string;
+//   references: ReferenceRoutes[];
+//   referencePhones: ReferencePhoneRoutes[];
+// }
+
+// export interface ReferencePhoneRoutes {
+//   id: number;
+//   name: string;
+//   phone: string;
+//   order: string;
+//   isActive: boolean;
+//   createdAt: string;
+//   updatedAt: string;
+// }
+
+// export interface ReferenceRoutes {
+//   id: number;
+//   description: string;
+//   link: string;
+//   image: string;
+//   order: string;
+//   isActive: boolean;
+//   createdAt: string;
+//   updatedAt: string;
+// }
+
 export interface Route {
   id: number;
   date: string;
@@ -8,11 +83,11 @@ export interface Route {
   createdAt: string;
   updatedAt: string;
   user: UserRoutes;
-  car: CarRoutes;
-  sellers: SellerRoutes[];
+  car: Car;
+  sellers: SellerElementRoutes[];
 }
 
-interface CarRoutes {
+interface Car {
   id: number;
   placa: string;
   image: string;
@@ -31,20 +106,29 @@ interface UserRoutes {
   password?: string;
 }
 
-interface SellerRoutes {
+interface SellerElementRoutes {
+  id: number;
+  order: number;
+  isActive: boolean;
+  createdAt: string;
+  updatedAt: string;
+  seller: SellerSellerRoutes;
+}
+
+interface SellerSellerRoutes {
   id: number;
   uuid: string;
   nombre: string;
-  calle: string;
-  numero: string;
-  colonia: string;
-  ciudad: string;
+  calle: null | string;
+  numero: null | string;
+  colonia: null | string;
+  ciudad: null | string;
   municipio: string;
   estado: string;
-  cp: string;
+  cp: null | string;
   linkUbicacion: string;
   image: string;
-  personaQueAtiende: null;
+  personaQueAtiende: string;
   isActive: boolean;
   createdAt: string;
   updatedAt: string;
@@ -65,8 +149,8 @@ export interface ReferencePhoneRoutes {
 export interface ReferenceRoutes {
   id: number;
   description: string;
-  link: string;
-  image: string;
+  link: null;
+  image: null;
   order: string;
   isActive: boolean;
   createdAt: string;
