4926f2a69825d27de298f6d372d6a91296f8d75e
Eduardo Dominguez <you@example.com[D[D[D[D[D[>
2023-08-04 17:24:22 -0600

    feat: add color menu

--- layouts/MainLayout/listItems.tsx
+++ layouts/MainLayout/listItems.tsx
@@ -13,90 +13,97 @@ import { userTypes } from "../../constants";
 import DirectionsCarIcon from "@mui/icons-material/DirectionsCar";
 import LocationCityIcon from "@mui/icons-material/LocationCity";
 import AssessmentIcon from "@mui/icons-material/Assessment";
+import { OverridableComponent } from "@mui/material/OverridableComponent";
+import { SvgIconTypeMap } from "@mui/material";
 
-export const MainListItems = () => {
+interface elmMenu {
+  name: string;
+  icon: OverridableComponent<SvgIconTypeMap<{}, "svg">> & { muiName: string };
+  path: string;
+  role?: string;
+}
+
+const menu: elmMenu[] = [
+  {
+    name: "Inicio",
+    icon: DashboardIcon,
+    path: "/dashboard",
+  },
+  {
+    name: "Sellers",
+    icon: ContactPhoneIcon,
+    path: "/sellers",
+    role: userTypes.ADMIN,
+  },
+  {
+    name: "Rutas",
+    icon: LocationOnIcon,
+    path: "/rutas",
+    role: userTypes.ADMIN,
+  },
+  {
+    name: "Camionetas",
+    icon: DirectionsCarIcon,
+    path: "/camionetas",
+    role: userTypes.ADMIN,
+  },
+  {
+    name: "Logisticas",
+    icon: LocationCityIcon,
+    path: "/logisticas",
+    role: userTypes.ADMIN,
+  },
+  {
+    name: "Reportes",
+    icon: AssessmentIcon,
+    path: "/reportes",
+    role: userTypes.ADMIN,
+  },
+  {
+    name: "Usuarios",
+    icon: PeopleAltIcon,
+    path: "/users",
+    role: userTypes.ADMIN,
+  },
+];
+
+const style = {
+  backgroundColor: "rgb(204, 204, 204) !important",
+  borderLeft: "8px solid #35A29F",
+  width: "calc(100% - 8px) !important",
+};
+
+const ComponentMenu = ({ name, icon: Icon, path, role }: elmMenu) => {
   const router = useRouter();
   const { user } = React.useContext(AuthContext);
   const userType = React.useMemo(() => {
     return user?.userType;
   }, [user]);
+
+  if (role && role !== userType) {
+    return null;
+  }
+
+  return (
+    <ListItemButton
+      onClick={() => router.push(path)}
+      selected={router.pathname === path}
+      style={router.pathname === path ? style : {}}
+    >
+      <ListItemIcon>
+        <Icon color={router.pathname === path ? "primary" : "inherit"} />
+      </ListItemIcon>
+      <ListItemText primary={name} />
+    </ListItemButton>
+  );
+};
+
+export const MainListItems = () => {
   return (
     <React.Fragment>
-      <ListItemButton
-        onClick={() => router.push("/dashboard")}
-        selected={router.pathname === "/dashboard"}
-      >
-        <ListItemIcon>
-          <DashboardIcon />
-        </ListItemIcon>
-        <ListItemText primary="Inicio" />
-      </ListItemButton>
-      {userType === userTypes.ADMIN && (
-        <ListItemButton
-          onClick={() => router.push("/sellers")}
-          selected={router.pathname === "/sellers"}
-        >
-          <ListItemIcon>
-            <ContactPhoneIcon />
-          </ListItemIcon>
-          <ListItemText primary="Sellers" />
-        </ListItemButton>
-      )}
-      {userType === userTypes.ADMIN && (
-        <ListItemButton
-          onClick={() => router.push("/rutas")}
-          selected={router.pathname === "/rutas"}
-        >
-          <ListItemIcon>
-            <LocationOnIcon />
-          </ListItemIcon>
-          <ListItemText primary="Rutas" />
-        </ListItemButton>
-      )}
-      {userType === userTypes.ADMIN && (
-        <ListItemButton
-          onClick={() => router.push("/camionetas")}
-          selected={router.pathname === "/camionetas"}
-        >
-          <ListItemIcon>
-            <DirectionsCarIcon />
-          </ListItemIcon>
-          <ListItemText primary="Camionetas" />
-        </ListItemButton>
-      )}
-      {userType === userTypes.ADMIN && (
-        <ListItemButton
-          onClick={() => router.push("/logisticas")}
-          selected={router.pathname === "/logisticas"}
-        >
-          <ListItemIcon>
-            <LocationCityIcon />
-          </ListItemIcon>
-          <ListItemText primary="Logisticas" />
-        </ListItemButton>
-      )}
-      {userType === userTypes.ADMIN && (
-        <ListItemButton
-          onClick={() => router.push("/reportes")}
-          selected={router.pathname === "/reportes"}
-        >
-          <ListItemIcon>
-            <AssessmentIcon />
-          </ListItemIcon>
-          <ListItemText primary="Reportes" />
-        </ListItemButton>
-      )}
-      {userType === userTypes.ADMIN && (
-        <ListItemButton
-          onClick={() => router.push("/users")}
-          selected={router.pathname === "/users"}
-        >
-          <ListItemIcon>
-            <PeopleAltIcon />
-          </ListItemIcon>
-          <ListItemText primary="Users" />
-        </ListItemButton>
-      )}
+      {menu.map((elm) => (
+        <ComponentMenu key={elm.name} {...elm} />
+      ))}
     </React.Fragment>
   );
 };
